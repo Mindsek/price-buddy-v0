9a83cd665db803807edb016c9952924389151c84
Timber <contactmindsek@gmail.com>
2025-03-18 11:00:43 +0100

    refactor: add redirection in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,12 +10,18 @@ import { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const { nextUrl } = request;
   const session = await auth();
+  console.log("session", session);
   const isAuthenticated = !!session?.user;
+  console.log("isAuthenticated", isAuthenticated);
 
   const isPublicRoute =
     (PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) ||
       nextUrl.pathname === ROOT) &&
     !PROTECTED_SUB_ROUTES.find((route) => nextUrl.pathname.includes(route));
+
+  if (isAuthenticated && nextUrl.pathname === LOGIN) {
+    return Response.redirect(new URL("/dashboard", nextUrl));
+  }
 
   if (!isAuthenticated && !isPublicRoute)
     return Response.redirect(new URL(LOGIN, nextUrl));
