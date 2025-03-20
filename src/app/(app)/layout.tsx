9b0c79bd1d7f920e2d86954334b85eb4a0732003
mindsek <contactmindsek@gmail.com>
2025-03-20 15:37:39 +0100

    refactor: separate layout in lib providers

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,33 +1,7 @@
-import { Session } from 'next-auth';
-import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
-
-import { auth } from '@/auth';
-import { AppSidebar } from '@/components/layout/sidebar/app-sidebar';
-import { LOGIN } from '@/constants/routes';
+import { SidebarAppProvider } from '@/lib/providers/SidebarAppProvider';
 
 export default async function AppLayout({ children }: { children: ReactNode }) {
-  const session = await auth();
-
-  if (!session?.user) {
-    redirect(LOGIN);
-  }
-
-  return (
-    <SidebarProvider>
-      <AppSidebar session={session as Session} />
-      <SidebarInset>
-        <div className='flex-1 overflow-hidden'>
-          <SidebarTrigger />
-          {children}
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <SidebarAppProvider>{children}</SidebarAppProvider>;
 }
