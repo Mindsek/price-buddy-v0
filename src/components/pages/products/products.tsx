9553a9c3db93b4ef6a75dea40c4f5e86ccf7eec3
mindsek <contactmindsek@gmail.com>
2025-03-21 12:36:15 +0100

    fix: handle no supermarkets when adding price

--- src/components/pages/products/products.tsx
+++ src/components/pages/products/products.tsx
@@ -5,7 +5,6 @@ import { Plus, Search } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { TableCell, TableRow } from '@/components/ui/table';
 
 import { AddPriceDialog } from './form/price/add-price-dialog';
 import { AddProductDialog } from './form/product/add-product-dialog';
@@ -67,15 +66,7 @@ export const ProductsPage = ({
       </Card>
 
       <AddProductDialog />
-      {supermarkets.length === 0 ? (
-        <TableRow>
-          <TableCell colSpan={5} className='text-center'>
-            Aucun produits trouvé
-          </TableCell>
-        </TableRow>
-      ) : (
-        <AddPriceDialog supermarkets={supermarkets} />
-      )}
+      <AddPriceDialog supermarkets={supermarkets} />
       <ProductDetail />
       <ProductDeletePrice />
     </div>
