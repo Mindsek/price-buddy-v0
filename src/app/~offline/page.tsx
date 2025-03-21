ed5c5cad81f084b4be78979f6c4734c6d58fee35
mindsek <contactmindsek@gmail.com>
2025-03-21 11:08:46 +0100

    refactor: sidebar offline

--- src/app/~offline/page.tsx
+++ src/app/~offline/page.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import { WifiOffIcon } from 'lucide-react';
+import { RefreshCcwIcon, WifiOffIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 
 import { OfflineLayoutProvider } from '@/lib/providers/offline-layout-provider';
+import { cn } from '@/lib/utils';
 
 export default function OfflinePage() {
   const router = useRouter();
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const handleRefresh = () => {
+    setIsRefreshing(true);
+    setTimeout(() => {
+      router.refresh();
+      setIsRefreshing(false);
+    }, 1000);
+  };
 
   return (
     <OfflineLayoutProvider>
@@ -18,7 +30,18 @@ export default function OfflinePage() {
         <p className='text-muted-foreground'>
           Vérifiez votre connexion internet et réessayez.
         </p>
-        <Button onClick={() => router.refresh()}>Réessayer</Button>
+        <Button
+          onClick={handleRefresh}
+          className={cn(isRefreshing && 'animate-pulse')}
+        >
+          Réessayer
+          <RefreshCcwIcon
+            className={cn(
+              'ml-2 h-4 w-4 transition-transform',
+              isRefreshing && 'animate-spin'
+            )}
+          />
+        </Button>
       </div>
     </OfflineLayoutProvider>
   );
--- src/lib/providers/offline-layout-provider.tsx
+++ src/lib/providers/offline-layout-provider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Session } from 'next-auth';
 import { ReactNode } from 'react';
 
 import {
@@ -8,12 +9,22 @@ import {
   SidebarTrigger,
 } from '@/components/ui/sidebar';
 
-import { BaseSidebar } from '@/components/layout/sidebar/base-sidebar';
+import { AppSidebar } from '@/components/layout/sidebar/app-sidebar';
 
 export function OfflineLayoutProvider({ children }: { children: ReactNode }) {
+  const session = {
+    user: {
+      id: 'offline',
+      name: 'Offline User',
+      email: 'offline.user@example.com',
+      image: '',
+    },
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(),
+  };
+
   return (
     <SidebarProvider>
-      <BaseSidebar />
+      <AppSidebar session={session as Session} />
       <SidebarInset>
         <div className='flex-1 overflow-hidden'>
           <SidebarTrigger />
